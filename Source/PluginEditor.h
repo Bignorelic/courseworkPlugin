4f6d0f6b75aea8c293e8f7a96c31b043f98adeec
Enrico Gubbini <EnricoGubbini@gmail.com>
2023-03-03 23:10:11 +0000

    spectrum button path is only created once

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -331,7 +331,27 @@ private:
 //==============================================================================
 
 struct PowerButton : juce::ToggleButton {};
-struct SpectrumButton : juce::ToggleButton {};
+struct SpectrumButton : juce::ToggleButton 
+{
+    void resized() override
+    {
+        auto bounds = getLocalBounds();
+        auto insetRect = bounds.reduced(4);
+
+        randomPath.clear();
+
+        juce::Random r;
+        randomPath.startNewSubPath(insetRect.getX(),
+            insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+
+        for (auto x = insetRect.getX() + 1; x < insetRect.getRight(); x += 2)
+        {
+            randomPath.lineTo(x,
+                insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+        }
+    }
+    juce::Path randomPath;
+};
 
 class CourseworkPluginAudioProcessorEditor  : public juce::AudioProcessorEditor, public juce::Timer
 {
